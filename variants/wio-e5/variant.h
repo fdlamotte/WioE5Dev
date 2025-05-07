dad8c537a2262a95df8a81627ee426d0155d7b01
Florent <florent@frizoncorrea.fr>
2025-05-07 21:48:42 +0200

    initial commit, some bits don't compile yet ...

---
+++ variants/wio-e5/variant.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// UART Definitions
+#ifndef SERIAL_UART_INSTANCE
+  #define SERIAL_UART_INSTANCE  101
+#endif
+
+//#include <variant_generic.h>
+#include <variant_LORA_E5_MINI.h>
+
+#undef RNG
